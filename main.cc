3ee05d08c949e11865388e3b555162fc4229dcb4
Mateusz Kojro <mateuszkojro@outlook.com>
2022-06-20 15:00:36 +0200

    Initial commit

---
+++ main.cc
@@ -0,0 +1,16 @@
+#include "example.pb.h"
+#include <iostream>
+#include <string>
+
+int main() {
+  example::Person person;
+  person.set_name("Mateusz");
+  person.set_fraction(12.1);
+  for (int i = 0; i < 10; i++) {
+    auto phone = person.add_phones();
+    phone->set_number(std::to_string(i));
+    phone->set_type(example::Person::PhoneType::Person_PhoneType_WORK);
+  }
+  std::cout << person.DebugString() << std::endl;
+  example::AddressBook::descriptor()
+}
---
+++ main.py
@@ -0,0 +1,7 @@
+import example_pb2 as example
+
+person = example.Person()
+
+person.name = "Mateusz"
+
+print(person)
